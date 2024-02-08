6edda4b426c19f74276fdfa3a1810b85dd41a818
victor <josevictornascimento2016@gmail.com>
2024-02-08 12:13:11 -0300

    feat: create route in WebSocket that returns the votes computed for an option in real time

--- src/ws/poll-results.ts
+++ src/ws/poll-results.ts
@@ -1,9 +1,17 @@
 import { FastifyInstance } from "fastify";
+import z from "zod";
+import { voting } from "../utils/voting-pub-sub";
 
 export async function pollResults(app: FastifyInstance) {
   app.get('/polls/:pollId/results', {websocket: true}, (connection, request) => {
-    connection.socket.on('message', (message: string) => {
-      connection.socket.send('you sent: ' + message)
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    voting.subscribe(pollId, (message) => {
+      connection.socket.send(JSON.stringify(message))
     })
   })
 }
